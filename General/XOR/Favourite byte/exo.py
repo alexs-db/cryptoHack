caea56a8b8d38f5f93fbea4ca9b58c555ff34e4a
Alexis <debra.alexis57645@gmail.com>
2024-04-09 17:58:22 +0200

    ..

--- General/XOR/Favourite byte/exo.py
+++ General/XOR/Favourite byte/exo.py
@@ -1,3 +1,11 @@
-#Work in Progress !
+numb = '73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d'
+byteVar = bytes.fromhex(numb)
+result = ""
 
-#Come back later ^^
+for byte in range(256):  # There are 256 possible bytes
+    decoded = ""
+    for i in range(len(byteVar)):
+        xor_result = byteVar[i] ^ byte
+        decoded += chr(xor_result)
+    print(f"With byte {byte:02x}: {decoded}")
+
